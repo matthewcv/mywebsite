04ac23a531957b7d01ad8d41d6043a927244924f
matthewcv <matthew@matthewcv.com>
2013-04-09 06:51:33 -0500

    cleaning, bootstrap, candy

--- mywebsite/App_Start/BundleConfig.cs
+++ mywebsite/App_Start/BundleConfig.cs
@@ -8,15 +8,6 @@ namespace mywebsite
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
 
 
             bundles.Add(new ScriptBundle("~/bundles/account").Include(
@@ -29,6 +20,7 @@ namespace mywebsite
                 ));
             bundles.Add(new ScriptBundle("~/bundles/candy").Include(
                         "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
                         "~/Scripts/knockout-{version}.js",
                         "~/Scripts/sammy-{version}.js",
                         "~/Scripts/toastr.js",
@@ -42,23 +34,12 @@ namespace mywebsite
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css",
                 "~/Content/toastr.css",
                 "~/Content/site.css"
                 ));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
